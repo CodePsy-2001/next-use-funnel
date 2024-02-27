1601932e725ee654a36954e009caf36db636f4a5
CodePsy-2001 <codepsy2001@gmail.com>
2024-02-27 15:25:19 +0900

    remove type `any`

--- mock/next-navigation.ts
+++ mock/next-navigation.ts
@@ -7,4 +7,10 @@ export const notFound = vi.fn();
 export const redirect = vi.fn().mockImplementation((url: string) => {
   memoryRouter.setCurrentUrl(url);
 });
-export const useSearchParams = () => new URLSearchParams(memoryRouter.query as any);
+export const useSearchParams = (): URLSearchParams =>
+  Object.entries(memoryRouter.query).reduce((acc, [key, value]) => {
+    if (value == null) return acc;
+    if (Array.isArray(value)) value.forEach((v) => acc.append(key, v));
+    else acc.append(key, value);
+    return acc;
+  }, new URLSearchParams());
