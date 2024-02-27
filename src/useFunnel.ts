55d2a30ca56e62f6f7d5a4fadb58891ce62d3f8f
CodePsy-2001 <codepsy2001@gmail.com>
2024-02-27 15:25:01 +0900

    remove useCallback

--- src/useFunnel.ts
+++ src/useFunnel.ts
@@ -1,5 +1,4 @@
 import { withState as _withState } from "./withState";
-import { useCallback } from "react";
 import { type RouteFunnelOptions, useRouteFunnel } from "./useRouteFunnel";
 import type { ISteps } from "./utils";
 
@@ -8,6 +7,6 @@ export function useFunnel<Steps extends ISteps>(
   options: RouteFunnelOptions<Steps> = {},
 ) {
   const [Funnel, setStep] = useRouteFunnel<Steps>(steps, options);
-  const withState = useCallback(_withState<Steps>([Funnel, setStep]), [Funnel, setStep]);
+  const withState = _withState<Steps>([Funnel, setStep]);
   return Object.assign([Funnel, setStep], { withState });
 }
