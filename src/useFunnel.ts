584b8e48715463c31beee2163f0d5d31bc785251
CodePsy-2001 <codepsy2001@gmail.com>
2024-02-27 15:03:26 +0900

    [fix]: options default `{}`

--- src/useFunnel.ts
+++ src/useFunnel.ts
@@ -1,12 +1,13 @@
 import { withState as _withState } from "./withState";
 import { useCallback } from "react";
-import { useRouteFunnel } from "./useRouteFunnel";
+import { type RouteFunnelOptions, useRouteFunnel } from "./useRouteFunnel";
 import type { ISteps } from "./utils";
 
 export function useFunnel<Steps extends ISteps>(
-  ...params: Parameters<typeof useRouteFunnel<Steps>>
+  steps: Steps,
+  options: RouteFunnelOptions<Steps> = {},
 ) {
-  const [Funnel, setStep] = useRouteFunnel<Steps>(...params);
+  const [Funnel, setStep] = useRouteFunnel<Steps>(steps, options);
   const withState = useCallback(_withState<Steps>([Funnel, setStep]), [Funnel, setStep]);
   return Object.assign([Funnel, setStep], { withState });
 }
