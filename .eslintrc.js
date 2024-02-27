1edc31d0cda439a2fcab87fea0427910115dc088
CodePsy-2001 <codepsy2001@gmail.com>
2024-02-27 15:32:07 +0900

    add args/vars IgnorePatterns

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,5 +27,12 @@ module.exports = {
   },
   rules: {
     "react/react-in-jsx-scope": "off",
-  }
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        argsIgnorePattern: "^_+$",
+        varsIgnorePattern: "^_+$",
+      },
+    ],
+  },
 };
